0832fbec935949f1e49201677c7e55d4fe86f15f
Eray Nurtekin <eray.nurtekin@hotmail.com>
2022-01-15 17:00:55 +0300

    Add project files.

---
+++ Largest_Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Number
+{
+    internal class Program
+    {
+        static int[] arr = { 1, 2, 43, 35, 234, 345, 61, 17 };
+
+        static int largest()
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Largest is given :" + largest());
+            Console.ReadLine();
+        }
+    }
+}
